84c1c2f25d045649b97691729d807baaa203b7c5
Julio Marques <julio.jcmarques@gmail.com>
2024-09-23 22:43:38 -0300

    First setups

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'site_setup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
