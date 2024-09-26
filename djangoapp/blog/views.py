656ced890ac28d2e8d8c99f3d6382e4dfb60eab8
Julio Marques <julio.jcmarques@gmail.com>
2024-09-26 18:42:49 -0300

    templates adjustments

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,8 +1,51 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
+
+
+def post(request):
+    # Supondo que posts esteja definido em algum lugar do seu código
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    # Defina suas tags aqui
+    tags = ["Testando", "Atenção", "Obrigado", "Educação", "Python"]
+
+    return render(
+        request,
+        'blog/pages/post.html',
+        {
+            'page_obj': page_obj,
+            'tags': tags,  # Adiciona as tags ao contexto
+        }
     )
