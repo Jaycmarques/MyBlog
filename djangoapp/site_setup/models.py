3b7236bef4705c3f071219eb699d97676b9f8187
Julio Marques <julio.jcmarques@gmail.com>
2024-09-25 21:36:00 -0300

    setup validators

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validade_png
+from utils.images import resize_image
 
 
 class MenuLink(models.Model):
@@ -23,12 +25,30 @@ class SiteSetup(models.Model):
         verbose_name_plural = 'Setup'
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=255)
+
     show_header = models.BooleanField(default=True)
     show_search = models.BooleanField(default=True)
     show_menu = models.BooleanField(default=True)
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/', blank=True, default='',
+        validators=[validade_png],
+
+    )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
