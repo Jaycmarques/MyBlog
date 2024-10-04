ce7c86c384ccafe68d97ba292241fc30d3f1039c
Julio Marques <julio.jcmarques@gmail.com>
2024-10-04 19:12:46 -0300

    working on models

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
@@ -18,6 +19,9 @@ class Tag(models.Model):
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     class Meta:
@@ -34,3 +38,80 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255,
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text='If box unchecked, page not available in the website.'
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255,
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text='If box unchecked, post not available in the website.'
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Check if you want the image cover also in the post content.'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+    )
+    tag = models.ManyToManyField(Tag, blank=True, default='')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
