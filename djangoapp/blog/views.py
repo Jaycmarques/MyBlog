6e489b0ba3fd7b57be544238a5f21a3450187679
Julio Marques <julio.jcmarques@gmail.com>
2024-10-18 18:43:44 -0300

    replacing category by CategoryListView

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -117,27 +117,46 @@ class CreatedByListView(PostListView):
 #         }
 #     )
 
+class CategoryListView(PostListView):
+    allow_empty = False
 
-def category(request, slug):
-    posts = Post.objects.get_published().filter(category__slug=slug)
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )
 
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].category.name} - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = (
+            f'{self.object_list[0].category.name}'  # type: ignore
+            ' - Categoria - '
+        )
+        ctx.update({
             'page_title': page_title,
+        })
+        return ctx
 
-        }
-    )
+
+# def category(request, slug):
+#     posts = Post.objects.get_published().filter(category__slug=slug)
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     if len(page_obj) == 0:
+#         raise Http404()
+
+#     page_title = f'{page_obj[0].category.name} - '
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+
+#         }
+#     )
 
 
 def tag(request, slug):
