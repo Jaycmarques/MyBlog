a75f375cf29c86ed352011bdd8d61a2b348b0194
Julio Marques <julio.jcmarques@gmail.com>
2024-10-18 19:31:39 -0300

    def post replaced by class PostDetailView

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,10 @@
-from typing import Any
+from typing import Any, Dict
 from django.contrib.auth.models import User
-from django.core.paginator import Paginator
 from django.http import Http404
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Post, Page
 from django.db.models import Q, QuerySet
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 PER_PAGE = 9
 
@@ -14,7 +13,7 @@ class PostListView(ListView):
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
     paginate_by = PER_PAGE
-    queryset = Post.objects.get_published()
+    queryset = Post.objects.get_published()  # type: ignore
 
     # def get_queryset(self):
     #     queryset = super().get_queryset()
@@ -29,21 +28,6 @@ class PostListView(ListView):
         })
         return context
 
-
-# def index(request):
-#     posts = Post.objects.get_published()
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             # 'page_title': 'Home - ',
-#         }
-#     )
 
 class CreatedByListView(PostListView):
     def __init__(self, **kwargs: Any) -> None:
@@ -85,38 +69,6 @@ class CreatedByListView(PostListView):
         return super().get(request, *args, **kwargs)
 
 
-# def created_by(request, author_pk):
-#     user = User.objects.filter(pk=author_pk).first()
-#     if user is None:
-#         raise Http404()  # Levanta 404 se o usuário não for encontrado
-
-#     # Obtém os posts do usuário
-#     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
-
-#     # Lógica para montar o nome completo do usuário
-#     if user.first_name and user.last_name:
-#         user_full_name = f'{user.first_name} {user.last_name}'
-#     elif user.first_name:
-#         user_full_name = user.first_name
-#     else:
-#         user_full_name = user.username
-
-#     page_title = f'{user_full_name} Posts - '
-
-#     # Paginação
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': page_title,
-#         }
-#     )
-
 class CategoryListView(PostListView):
     allow_empty = False
 
@@ -137,27 +89,6 @@ class CategoryListView(PostListView):
         return ctx
 
 
-# def category(request, slug):
-#     posts = Post.objects.get_published().filter(category__slug=slug)
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     if len(page_obj) == 0:
-#         raise Http404()
-
-#     page_title = f'{page_obj[0].category.name} - '
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': page_title,
-
-#         }
-#     )
-
 class TagListView(PostListView):
     allow_empty = False
 
@@ -177,32 +108,6 @@ class TagListView(PostListView):
         })
         return ctx
 
-
-# def tag(request, slug):
-#     # Filtra os posts pela tag
-#     posts = Post.objects.get_published().filter(tags__slug=slug)
-
-#     # Levanta 404 se não houver posts
-#     if not posts.exists():
-#         raise Http404("No posts found for this tag.")
-
-#     # Paginação
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     # Define o título da página usando a tag do primeiro post
-#     first_post_tag = page_obj[0].tags.first()
-#     page_title = f'{first_post_tag.name} - ' if first_post_tag else 'Tag - '
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': page_title,
-#         }
-#     )
 
 class SearchListView(PostListView):
     def __init__(self, *args, **kwargs):
@@ -236,73 +141,41 @@ class SearchListView(PostListView):
         return super().get(request, *args, **kwargs)
 
 
-# def search(request):
-#     search_value = request.GET.get('search', '').strip()
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
 
-#     # Verifica se o search_value não está vazio
-#     if search_value:
-#         posts = (Post.objects.get_published()
-#                  .filter(
-#                      Q(title__icontains=search_value) |
-#                      Q(excerpt__icontains=search_value) |
-#                      Q(content__icontains=search_value)
-#         ))
-#     else:
-#         posts = Post.objects.none()  # Retorna uma QuerySet vazia se não houver valor de busca
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+        page = self.get_object()
+        page_title = f'{page.title} - Page - '  # type: ignore
+        ctx.update({
+            'page_title': page_title,
 
-#     # Definindo o título da página
-#     page_title = f'Search results for: {search_value[:30]} - ' if search_value else 'Search - '
+        })
+        return ctx
 
-#     # Paginação
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,  # Passa o objeto paginado
-#             'search_value': search_value,
-#             'page_title': page_title,
-#         }
-#     )
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(is_published=True)
 
 
-def page(request, slug):
-    # Tenta obter a página correspondente ao slug. Se não existir, retorna 404.
-    page_obj = get_object_or_404(Page, is_published=True, slug=slug)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/pages/post.html'
+    slug_field = 'slug'
+    context_object_name = 'post'
 
-    # Define o título da página com o título da página
-    page_title = f'{page_obj.title} - '  # Personalize conforme necessário
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+        post = self.get_object()
+        page_title = f'{post.title} - Post - '  # type: ignore
+        ctx.update({
+            'page_title': page_title,
 
-    return render(
-        request,
-        'blog/pages/page.html',
-        {
-            'page': page_obj,
-            'show_description': False,
-            'page_title': page_title,  # Adiciona o título da página ao contexto
-        }
-    )
+        })
+        return ctx
 
-
-def post(request, slug):
-    # Tenta obter o post correspondente ao slug. Se não existir, retorna 404.
-    post_obj = get_object_or_404(Post.objects.get_published(), slug=slug)
-
-    # Se o modelo Post tiver uma relação ManyToMany com Tag
-    tags = post_obj.tags.all()  # Isso obtém todas as tags associadas ao post
-
-    # Define o título da página com o título do post
-    page_title = f'{post_obj.title} - '
-
-    return render(
-        request,
-        'blog/pages/post.html',
-        {
-            'post': post_obj,
-            'tags': tags,
-            'page_title': page_title,  # Adiciona o título da página ao contexto
-        }
-    )
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(is_published=True)
