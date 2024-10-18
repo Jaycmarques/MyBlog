61b86eb7f517ea328e1d0095c019799cf177ea77
Julio Marques <julio.jcmarques@gmail.com>
2024-10-18 18:31:45 -0300

    replacing created_by by CreatedByListView

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,17 +4,15 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Page
-from django.db.models import Q
+from django.db.models import Q, QuerySet
 from django.views.generic import ListView
 
 PER_PAGE = 9
 
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()
 
@@ -47,38 +45,77 @@ class PostListView(ListView):
 #         }
 #     )
 
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
 
-def created_by(request, author_pk):
-    user = User.objects.filter(pk=author_pk).first()
-    if user is None:
-        raise Http404()  # Levanta 404 se o usuário não for encontrado
-
-    # Obtém os posts do usuário
-    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
-
-    # Lógica para montar o nome completo do usuário
-    if user.first_name and user.last_name:
-        user_full_name = f'{user.first_name} {user.last_name}'
-    elif user.first_name:
-        user_full_name = user.first_name
-    else:
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
         user_full_name = user.username
 
-    page_title = f'{user_full_name} Posts - '
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = user_full_name + " - Posts "
 
-    # Paginação
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
+
+        return ctx
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
+
+
+# def created_by(request, author_pk):
+#     user = User.objects.filter(pk=author_pk).first()
+#     if user is None:
+#         raise Http404()  # Levanta 404 se o usuário não for encontrado
+
+#     # Obtém os posts do usuário
+#     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+
+#     # Lógica para montar o nome completo do usuário
+#     if user.first_name and user.last_name:
+#         user_full_name = f'{user.first_name} {user.last_name}'
+#     elif user.first_name:
+#         user_full_name = user.first_name
+#     else:
+#         user_full_name = user.username
+
+#     page_title = f'{user_full_name} Posts - '
+
+#     # Paginação
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+#         }
+#     )
 
 
 def category(request, slug):
